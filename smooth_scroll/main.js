9206c18a5995cb79a0d1dfc9ca8faebfadfa6010
wide8550 <wide8550@gmail.com>
2020-06-07 15:07:42 +0800

    smooth scroll landing page

---
+++ smooth_scroll/main.js
@@ -0,0 +1,14 @@
+// Option 2 - jquery Smooth Scroll
+$('.navbar ul a').on('click', function (e) {
+  // console.log(this.hash);
+  if (this.hash != '') {
+    e.preventDefault();
+    const hash = this.hash;
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      800
+    );
+  }
+});
